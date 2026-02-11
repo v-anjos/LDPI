971ee912a1044c3dc3f7ac25c1cd4d46ea428568
Vinicius de Araujo dos Anjos <vanjos990@gmail.com>
2026-02-10 22:53:32 -0300

    Add files via upload

---
+++ beecrowd/Homework-INICIAL/Area do Circulo.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+	double A=0.0;
+	double r, n = 3.14159;
+	
+	scanf("%lf", &r);	
+	
+	A = n * (r*r);
+		
+	printf ("A=%.4f\n", A);
+	
+    return 0;
+ 
+    return 0;
+}
