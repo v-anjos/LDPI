408c50c49ced27158395f29a28b4515666e27ef9
Vinicius de Araujo dos Anjos <vanjos990@gmail.com>
2026-03-04 21:10:54 -0300

    Update O Maior.c

--- beecrowd/lista2/O Maior.c
+++ beecrowd/lista2/O Maior.c
@@ -1,4 +1,4 @@
-#include <stdio.h> //inclusão das bibliotecas, math.n será biblioteca que nos permitira ver o valor absoluto da diferença entre dois numeros
+#include <stdio.h> //inclusão das bibliotecas, math.h será a biblioteca que nos permitira ver o valor absoluto da diferença entre dois numeros
 #include <math.h>
 
 int main(){
@@ -9,7 +9,7 @@ int main(){
 	MaiorAB = (a + b + abs(a-b))/2; //Calculo do maior valor entre os valores armazenados em a e b, armazenado em MaiorAB
 	
 	MaiorAB = (c + MaiorAB + abs(c - MaiorAB))/2; //calculo do maior valor entre os valores armazenao em C e MaiorAB, armazeando novamente em MaiorAB
-	printf("%d eh o maior\n", MaiorAB); // impresão na tela da mior valos entre os 3 digitados
+	printf("%d eh o maior\n", MaiorAB); // impressão na tela do maior valor entre os 3 digitados
 	
 	return 0;
 }
