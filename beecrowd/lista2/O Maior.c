f6da86b708cb9e3f82675f91e272a9366513cd3d
Vinicius de Araujo dos Anjos <vanjos990@gmail.com>
2026-03-04 21:10:03 -0300

    Update O Maior.c

--- beecrowd/lista2/O Maior.c
+++ beecrowd/lista2/O Maior.c
@@ -4,12 +4,12 @@
 int main(){
 	int cont, MaiorAB, a, b, c; //Declaração de variaveis 
 	
-	scanf("%d %d %d", &a, &b, &c); //Leitura dos valores armazenados nass variaveis
+	scanf("%d %d %d", &a, &b, &c); //Leitura dos valores armazenados nas variaveis
 	
 	MaiorAB = (a + b + abs(a-b))/2; //Calculo do maior valor entre os valores armazenados em a e b, armazenado em MaiorAB
 	
 	MaiorAB = (c + MaiorAB + abs(c - MaiorAB))/2; //calculo do maior valor entre os valores armazenao em C e MaiorAB, armazeando novamente em MaiorAB
-	printf("%d eh o maior\n", MaiorAB);
+	printf("%d eh o maior\n", MaiorAB); // impresão na tela da mior valos entre os 3 digitados
 	
 	return 0;
 }
