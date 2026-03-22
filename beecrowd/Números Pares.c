ad34ac83c2870b3a5d39be49fb701a6d8ae43e80
Vinicius de Araujo dos Anjos <vanjos990@gmail.com>
2026-03-21 22:50:52 -0300

    Create Números Pares.c

---
+++ beecrowd/Números Pares.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int A;
+
+    for ( A = 1; A <=100; A++)
+    {
+        if (A % 2 == 0)
+        {
+            printf("%d\n", A);
+        }
+    }
+    
+
+    return 0;
+}
