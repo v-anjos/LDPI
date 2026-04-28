0ac81669875e60ff37c3ff9d0337b0754f1d0b1a
Vinicius de Araujo dos Anjos <vanjos990@gmail.com>
2026-04-28 20:11:25 -0300

    Fix indentation and comments in escada contrario.c

--- escada contrario.c
+++ escada contrario.c
@@ -4,11 +4,11 @@ int main() {
     int i, j, w, n;
     scanf("%d", &n);
 
-    for (i = 1; i <= n; i++) {
-        for (j = 1; j <= n - i; j++) {
+    for (i = 1; i <= n; i++) { //percorrer as linhas
+        for (j = 1; j <= n - i; j++) { //epaços vzios
             printf(" ");
         }
-        for (w = 1; w <= i; w++) {
+        for (w = 1; w <= i; w++) { //preencher as linha
             printf("*");
         }
         printf("\n");
